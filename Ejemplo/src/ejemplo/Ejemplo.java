b24e6581335ee3b1f6452a2b3b874b1050f6fd1c
Shbs <Shbs@SHBS>
2017-03-26 13:20:50 -0500

    PROYECTO Y CLASES_EJEMPLO

--- Ejemplo/src/ejemplo/Ejemplo.java
+++ Ejemplo/src/ejemplo/Ejemplo.java
@@ -16,6 +16,8 @@ public class Ejemplo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //PRIMERA LINEA DE CODIGO
     }
     
 }
